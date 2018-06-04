f1c43529cfb6a890380c57e0dcb5b2fc661d6515
rogercodes1 <roger.ucsb@gmail.com>
2018-06-04 15:07:49 -0400

    push them files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Board from './Board'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<Board count={50}/>, document.getElementById('root'))
+registerServiceWorker()
+
+//Make the DIV element draggagle:
